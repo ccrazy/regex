a51d1b3d57e18c2e31d3f65484041e4dfd4ae28b
deyin liu <deyinliu@deyindeMacBook-Pro.local>
2017-03-11 20:06:35 +0800

    test

--- test.php
+++ test.php
@@ -1,1 +1,8 @@
 <?php
+$patten = array("/\d/","/[a-z]/","/[1a]/");
+$repalce = array("a","b","c");
+$subject = array("1","a","3","wd","5","a2");
+
+$r = preg_filter($patten,$repalce,$subject);
+
+print_r($r);
